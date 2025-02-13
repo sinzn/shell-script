ddfc5b2047534b36843ebffc647f64916eb5a3aa
sinzn <shivam.sinzh@gmail.com>
2025-02-13 11:12:39 +0530

    This is third commit which we are commit to hello.sh

---
+++ hello.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Gabdar : kitne admi the."
+
+echo "Kaliya : 3 sarkar. "
+
+echo "Gabbar : kitne baje hai "
+
+date 
+
+# This is solye by tws
+
+<<comment
+This 
+is 
+multi
+echo "This comment is not showing"
+line 
+comment
+
+echo "  THis is solye by tws "
+
+echo "gabbar : kitne der se machine chal rahi hai "
+
+uptime 
+
